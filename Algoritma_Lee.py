efe4a144800506f570967f4aa6a65acf6d2154af
Balya Malkanul Karim <balya.karim@gmail.com>
2020-08-23 09:38:57 +0700

    Update Algoritma_Lee.py

--- Algoritma_Lee.py
+++ Algoritma_Lee.py
@@ -1,38 +1,60 @@
 from collections import deque
 
 
-baris = [-1, 0, 0, 1]
-kolom = [0, -1, 1, 0]
+# Below lists details all 4 possible movements from a cell
+row = [-2, 1, 1, 1]
+col = [0, -1, 1, 0]
 
 
+# Function to check if it is possible to go to position (row, col)
+# from current position. The function returns false if row, col:
+# is not a valid position or has value 0 or it is already visited
 def isValid(mat, visited, row, col):
-	return (baris >= 0) and (baris < M) and (kolom >= 0) and (kolom < N) \
+	return (row >= 0) and (row < M) and (col >= 0) and (col < N) \
 		   and mat[row][col] == 1 and not visited[row][col]
 
 
+# Find Shortest Possible Route in a matrix mat from source
+# cell (i, j) to destination cell (x, y)
 def BFS(mat, i, j, x, y):
 
+	# construct a matrix to keep track of visited cells
 	visited = [[False for x in range(N)] for y in range(M)]
 
+	# create an empty queue
 	q = deque()
 
+	# mark source cell as visited and enqueue the source node
 	visited[i][j] = True
 
+	# (i, j, dist) represents matrix cell coordinates and its
+	# minimum distance from the source
 	q.append((i, j, 0))
 
+	# stores length of longest path from source to destination
 	min_dist = float('inf')
 
+	# loop till queue is empty
 	while q:
 
+		# pop front node from queue and process it
 		(i, j, dist) = q.popleft()
 
+		# (i, j) represents current cell and dist stores its
+		# minimum distance from the source
+
+		# if destination is found, update min_dist and stop
 		if i == x and j == y:
 			min_dist = dist
 			break
 
+		# check for all 4 possible movements from current cell
+		# and enqueue each valid movement
 		for k in range(4):
+			# check if it is possible to go to position
+			# (i + row[k], j + col[k]) from current position
 			if isValid(mat, visited, i + row[k], j + col[k]):
-
+				# mark next cell as visited and enqueue it
 				visited[i + row[k]][j + col[k]] = True
 				q.append((i + row[k], j + col[k], dist + 1))
 
@@ -42,8 +64,10 @@ def BFS(mat, i, j, x, y):
 		print("Destination can't be reached from given source")
 
 
+# Shortest path in a Maze
 if __name__ == '__main__':
 
+	# input maze
 	mat = [
 		[1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
 		[0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
@@ -57,6 +81,8 @@ if __name__ == '__main__':
 		[0, 0, 1, 0, 0, 1, 1, 0, 0, 1]
 	]
 
+	# M x N matrix
 	M = N = 10
 
+	# Find shortest path from source (0, 0) to destination (7, 5)
 	BFS(mat, 0, 0, 7, 5)
